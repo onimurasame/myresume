dccd7425ab436028def36bfc1ccbee74ad717c7c
Raul Astudillo <onimurasame@gmail.com>
2019-01-02 15:48:05 -0800

    Added Language for future I18n support. Configured logging. Added Spring Data RestMVC

---
+++ src/main/kotlin/com/onimurasame/myresume/data/Language.kt
@@ -0,0 +1,8 @@
+package com.onimurasame.myresume.data
+
+enum class Language {
+    EN_US,
+    EN_UK,
+    ES_ES,
+    ES_CL
+}
--- src/main/kotlin/com/onimurasame/myresume/data/Resume.kt
+++ src/main/kotlin/com/onimurasame/myresume/data/Resume.kt
@@ -6,17 +6,18 @@ import org.springframework.data.annotation.CreatedBy
 import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.annotation.LastModifiedBy
 import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.annotation.Version
 import org.springframework.data.couchbase.core.mapping.Document
 import org.springframework.data.couchbase.core.mapping.id.GeneratedValue
 import org.springframework.data.couchbase.core.mapping.id.GenerationStrategy
 import java.io.Serializable
 import java.util.Date
-import javax.persistence.Version
 import javax.validation.constraints.NotNull
 
 @Document
 data class Resume(
     @Id @Field @GeneratedValue(strategy = GenerationStrategy.UNIQUE) val resumeId: Long,
+    @Field @NotNull val language: Language,
     @Field @NotNull val user: User,
     @Version @Field val version: Long?,
     @LastModifiedBy @Field val lastModifiedBy: String?,
